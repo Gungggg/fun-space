11f848e6378fa5760929e1518e1a4aaf330ff3df
Gungggg <agungsetyadi148@gmail.com>
2025-06-30 14:29:57 +0800

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+def luas_segitiga(alas, tinggi):
+    alas = int(input("Masukkan alas segitiga: "))
+    tinggi = int(input("Masukkan tinggi segitiga: "))
+    luas_segitiga = 0.5 * alas * tinggi
+    print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas_segitiga}.")
+    
+if __name__ == "__main__":
+    luas_segitiga()
